d552aa335449cee70504ff44c654002b1a58e12a
zaxel <sae11.z.alex@gmail.com>
2020-05-09 23:05:41 +0100

    main tamplate created

---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className="content">
+        <div>
+          <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" />
+        </div>
+        <div>
+          <img src="https://dyl80ryjxr1ke.cloudfront.net/external_assets/hero_examples/hair_beach_v1785392215/result.jpeg" />
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
