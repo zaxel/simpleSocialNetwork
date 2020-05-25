e01ef5c59fd693ac7ba22924d554c06acefa3771
zaxel <sae11.z.alex@gmail.com>
2020-05-26 00:30:21 +0100

    user profile logo added

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import * as axios from 'axios';
 import { connect } from 'react-redux';
-import {setAuthUserData} from './../../redux/AuthReducer';
+import {setAuthUserData, setUserProfileLogo} from './../../redux/AuthReducer';
 
 
 class HeaderContainer extends React.Component {
@@ -14,23 +14,46 @@ class HeaderContainer extends React.Component {
       if(response.data.resultCode === 0){
         let {id, login, email} = response.data.data;
         this.props.setAuthUserData(id, login, email);
+        getUserLogo(id);
       }
         // debugger;
     });
+    let getUserLogo = (id) => {
+      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${id}`).then(response => {
+        this.props.setUserProfileLogo(response.data.photos.small);
+      });
+    };
+   
+    
   }
-
+  
+  
 
   render(){
     return (
         <Header {...this.props}/>
       );
+      debugger;
   }
 }
 
+// getUserLogo = (id) => {
+//   console.log(id);
+//   // let userId = this.props.id;
+//   debugger;
+//   // if(!userId){userId=2}
+//   //   axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(response => {
+//   //       // debugger;
+//   //       this.props.setUserProfile(response.data);
+//   //   });
+// }
+
 const mapStateToProps = (state) =>({ 
   isAuth: state.auth.isAuth,
-  login: state.auth.login
+  login: state.auth.login,
+  id: state.auth.id,
+  profileIconSrc: state.auth.profileIconSrc
 });
 
 
-export default connect(mapStateToProps, {setAuthUserData})(HeaderContainer);
+export default connect(mapStateToProps, {setAuthUserData, setUserProfileLogo})(HeaderContainer);
--- src/redux/AuthReducer.js
+++ src/redux/AuthReducer.js
@@ -1,17 +1,20 @@
 
 const SET_USERS_DATA = 'SET-USERS-DATA';
+const SET_USER_PROFILE_LOGO = 'SET-USER-PROFILE-LOGO';
 
 
 let initialiseState = {
     id: null,
     login: null,
     email: null,
+    profileIconSrc: null,
     isFetching: false,
     isAuth: false
 
 }
 
 const AuthReducer = (state = initialiseState, action) => {
+   
     switch(action.type){
        case SET_USERS_DATA:
         return {
@@ -22,12 +25,23 @@ const AuthReducer = (state = initialiseState, action) => {
             
         };
 
+        case SET_USER_PROFILE_LOGO:
+        return {
+            
+            ...state, 
+            profileIconSrc: action.profileIconSrc
+            
+        };
+        
         default:
             return state;
+            
+
     }
 }
 
 export const setAuthUserData = (id, login, email)=>({type: SET_USERS_DATA, data : {id, login, email}});
+export const setUserProfileLogo = (src)=>({type: SET_USER_PROFILE_LOGO, profileIconSrc : src});
 
 
 
