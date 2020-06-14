fee96fa2b5f0d43a2ebbf9d6ab34af5e1e771544
zaxel <sae11.z.alex@gmail.com>
2020-06-14 15:13:32 +0100

    deleted unused lines

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import {Route, withRouter} from "react-router-dom";
-import Settings from "./components/Settings/Settings";
-import News from "./components/News/News";
-import Music from "./components/Music/Music";
 import UsersContainer from "./components/Users/UsersContainer";
 import LoginPage from "./components/Login/Login";
+import Settings from "./components/Settings/Settings";
+import Music from "./components/Music/Music";
 import { compose } from "redux";
 import {connect, Provider} from 'react-redux';
 import {initializeApp} from './redux/AppReducer';
 import Preloader from "./components/common/Preloader/Preloader";
 import {BrowserRouter} from "react-router-dom";
 import store from './redux/ReduxStore';
+import {withSuspense} from './hoc/withSuspense';
+
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const News = React.lazy(() => import('./components/News/News'));
 
 
 
@@ -30,22 +32,26 @@ class App extends React.Component {
     
     return (
       // <BrowserRouter>
-        <div className="app-wrapper">
-          <HeaderContainer />
-          <Navbar />
-          
-          <div className="app-wrapper-content">
-            <Route path='/profile/:userId?' render={()=> <ProfileContainer 
-              store={this.props.store}/>}/>
-            <Route path='/dialogs' render={()=> <DialogsContainer 
-              store={this.props.store} />}/>
-            <Route path='/news' render={()=> <News/>}/>
-            <Route path='/music' render={()=> <Music/>}/>
-            <Route path='/users' render={()=> <UsersContainer/>}/>
-            <Route path='/settings' render={()=> <Settings/>}/>
-            <Route path='/login' render={()=> <LoginPage/>}/>
-          </div>
+      <div className="app-wrapper">
+        <HeaderContainer />
+        <Navbar />
+
+        <div className="app-wrapper-content">
+          <Route
+            path="/profile/:userId?"
+            render={withSuspense(ProfileContainer, {store: this.props.store})}
+          />
+          <Route
+            path="/dialogs"
+            render={withSuspense(DialogsContainer, {store: this.props.store})}
+          />
+          <Route path="/news" render={() => <News />} />
+          <Route path="/music" render={() => <Music />} />
+          <Route path="/users" render={() => <UsersContainer />} />
+          <Route path="/settings" render={() => <Settings />} />
+          <Route path="/login" render={() => <LoginPage />} />
         </div>
+      </div>
       // </BrowserRouter>
     );
   }
