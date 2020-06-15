0d9c81c59d8b07e7134dcadaa1fc8f468f5a2f38
zaxel <sae11.z.alex@gmail.com>
2020-06-15 11:46:05 +0100

    componrntDidMount implemented in Profile container, to fix bug with profile update

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -7,7 +7,7 @@ import { compose } from "redux";
 
 class ProfileContainer extends React.Component {
 
-  componentDidMount(){
+  refreshProfile(){
     let userId = this.props.match.params.userId;
     if(!userId){
       userId=this.props.authorizedUserId
@@ -18,6 +18,15 @@ class ProfileContainer extends React.Component {
 
     this.props.setProfile(userId);
     this.props.setUserStatus(userId);
+  }
+
+  componentDidMount(){
+    this.refreshProfile();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+    if(this.props.match.params.userId != prevProps.match.params.userId)
+    this.refreshProfile();
   }
 
 
